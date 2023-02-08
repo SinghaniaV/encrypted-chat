70809c0f7b530fa7df1f1cadea59c190a77aca6e
SinghaniaV <vishsinghania@outlook.com>
2023-02-08 12:38:11 +0530

    added a feature to connect to supplied IP and port

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import socket
 import threading
 
-# for asymmetric encryption
+# library for implementing RSA encryption
 import rsa
 
 public_key, private_key = rsa.newkeys(1024)
@@ -9,27 +9,47 @@ public_partner = None
 
 choice = input("Do you want to host (1) or to connect (2): ")
 
-## getting the hostname by socket.gethostname() method
+# getting the hostname
 hostname = socket.gethostname()
-## getting the IP address using socket.gethostbyname() method
+# getting the IP address
 ip_address = socket.gethostbyname(hostname)
 
-print(f" your IP address: {ip_address}")
 
 if choice == "1":
+	# using IPv4 and TCP on port choosen by the OS (0)
 	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	server.bind((ip_address, 9999))
+	server.bind((ip_address, 0))
 	server.listen()
-    
+
+	print(f" Server listening on IP address: {ip_address} and port: {server.getsockname()[1]}")
+	
 	client, client_address = server.accept()
-	print(f" user with IP address: {client_address[0]} has connected")
+	print(f" A user with IP address: {client_address[0]} has connected!")
+
+	# sending a welcome message
+	client.send(bytes(f"Connected to {ip_address}", "utf-8"))
+	# sending our public key to the client
 	client.send(public_key.save_pkcs1("PEM"))
+
+	# storing the recieved public key from the client
 	public_partner = rsa.PublicKey.load_pkcs1(client.recv(1024))
 
 elif choice == "2":
+	# using IPv4 and TCP
 	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	client.connect((ip_address, 9999))
+
+	# getting the IP address of the host client to connect
+	server_ip, server_port = input("Please enter the (IP address<space>Port) to which you want to connect: ").split()
+
+	# connecting to IP address and port provided by the client
+	client.connect((server_ip, int(server_port)))
+
+	# accepting and printing the recieved message from the server
+	print(client.recv(1024).decode("utf-8"));
+	# accepting and storing the recieved public key from the server
 	public_partner = rsa.PublicKey.load_pkcs1(client.recv(1024))
+
+	# sending our public key to the server
 	client.send(public_key.save_pkcs1("PEM"))
 
 else:
